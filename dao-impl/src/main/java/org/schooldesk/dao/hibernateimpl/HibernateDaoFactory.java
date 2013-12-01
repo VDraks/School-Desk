3a1bde96d55d6c253b466488c8ded8ace786b8e7
Avenlisp <avenlisp@gmail.com>
2013-12-01 20:22:12 +0400

    Добавлено описание для конфигурации HibernateDaoFactory

--- dao-impl/src/main/java/org/schooldesk/dao/hibernateimpl/HibernateDaoFactory.java
+++ dao-impl/src/main/java/org/schooldesk/dao/hibernateimpl/HibernateDaoFactory.java
@@ -7,12 +7,21 @@ import org.hibernate.*;
 import org.schooldesk.dao.*;
 
 
+/**
+ * A DAO factory based on hibernate implementation
+ */
 public class HibernateDaoFactory extends DaoFactory implements IDaoFactory {
 	private static final String CONFIG_FILE = "dao.factory.config";
 
 	private Map<Class<? extends IDao<?>>, IDao<?>> daoPool = new HashMap<>();
 	private SessionFactory sessionFactory;
 
+	/**
+	 * Constructor for the DAO factory<br/>
+	 * Needs <b>dao.factory.config</b> for the factory configuration with
+	 * properties <b>db_login</b>, <b>db_password</b>, <b>db_connection</b> set.
+	 * @throws IOException if config is missing or can't be read
+	 */
 	public HibernateDaoFactory() throws IOException {
 		Properties configuration = getFactoryConfiguration();
 		sessionFactory = HibernateConfiguration.buildSessionFactory(
