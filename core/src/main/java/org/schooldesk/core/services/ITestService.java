e9a2a0ce1c99c7493ff36a2fcdd9e94dbbf4cfed
petrishchev <s.s.petrishchev@yandex.ru>
2013-12-01 15:57:56 +0400

    Добавлен метод для получения всех EducationStages

--- core/src/main/java/org/schooldesk/core/services/ITestService.java
+++ core/src/main/java/org/schooldesk/core/services/ITestService.java
@@ -1,6 +1,7 @@
 package org.schooldesk.core.services;
 
 import org.schooldesk.core.models.*;
+import org.schooldesk.dao.DataAccessException;
 
 import java.util.Set;
 
@@ -8,6 +9,7 @@ import java.util.Set;
 public interface ITestService extends IService {
 	// TODO: add CRUD methods
 
+	Set<EducationStageModel> getAllAvailableEducationStages() throws DataAccessException;
 	Set<CourseModel> getCourses(long stageId);
 	Set<CourseSectionModel> getCourseSections(long courseId);
 	TestModel getTest(long courseSectionId);
