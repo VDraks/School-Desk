71143a189bd27e74aa74f3f17c0fb64ca4c680db
Daniil Filippov <flipp5b@gmail.com>
2013-12-08 00:36:34 +0400

    В интеграционных тестах mock'и заменены на имплементации из dao-impl

---
+++ dao-impl/src/main/java/org/schooldesk/DbHelper.java
@@ -0,0 +1,10 @@
+package org.schooldesk;
+
+// TODO: place me in more appropriate package (using move refactoring), please
+public class DbHelper {
+	private DbHelper() {}
+
+	public static void clearDb() {
+		// TODO: implement me
+	}
+}
--- integration-tests/src/main/java/org/schooldesk/intergrationtests/AbstractTest.java
+++ integration-tests/src/main/java/org/schooldesk/intergrationtests/AbstractTest.java
@@ -1,16 +1,12 @@
 package org.schooldesk.intergrationtests;
 
+import org.junit.After;
 import org.junit.Before;
+import org.schooldesk.DbHelper;
 import org.schooldesk.core.services.IServiceFactory;
 import org.schooldesk.core.services.ServiceFactory;
-import org.schooldesk.dao.DataAccessException;
+import org.schooldesk.dao.DaoFactory;
 import org.schooldesk.dao.IDaoFactory;
-import org.schooldesk.dao.IUserDao;
-import org.schooldesk.dto.IUser;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
 public class AbstractTest {
@@ -19,23 +15,13 @@ public class AbstractTest {
 
 	@Before
 	public final void init() {
-		// TODO: use DaoFactory.create() when dao-impl will be adjusted
-		IUser user = mock(IUser.class);
-		when(user.getId()).thenReturn(42L);
-
-		IUserDao userDao = mock(IUserDao.class);
-		when(userDao.createDto()).thenReturn(user);
-		try {
-			when(userDao.save(any(IUser.class))).thenReturn(user);
-		}
-		catch (DataAccessException e) {
-			// never happens
-		}
-
-		daoFactory = mock(IDaoFactory.class);
-		when(daoFactory.getDao(IUserDao.class)).thenReturn(userDao);
-
+		daoFactory = DaoFactory.create();
 		serviceFactory = ServiceFactory.create(daoFactory);
+	}
+
+	@After
+	public final void dispose() {
+		DbHelper.clearDb();
 	}
 
 	protected IDaoFactory getDaoFactory() {
