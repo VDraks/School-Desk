2e2d12f3132f26088322ce529abb4cab2c1df4ec
andrey <pigarev.study@yandex.ru>
2013-12-09 15:52:12 +0400

    #26 temporary fix for some core classes. Only four classes added to list of annotated for now

--- dao-impl/src/main/java/org/schooldesk/dao/hibernateimpl/HibernateConfiguration.java
+++ dao-impl/src/main/java/org/schooldesk/dao/hibernateimpl/HibernateConfiguration.java
@@ -1,9 +1,13 @@
 package org.schooldesk.dao.hibernateimpl;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 import org.schooldesk.hibernateobjects.CourseCore;
+import org.schooldesk.hibernateobjects.CourseSectionCore;
+import org.schooldesk.hibernateobjects.ResourceCore;
+import org.schooldesk.hibernateobjects.TestCore;
 
 import java.io.File;
 import java.net.URL;
@@ -21,9 +25,10 @@ public class HibernateConfiguration {
 		AnnotationConfiguration config = new AnnotationConfiguration();
 		CONFIGURATION = config;
 		CONFIGURATION.setProperties(makeCommonProperties());
-		for (Class<?> annotatedClass : getClassesInPackage("org.schooldesk.hibernateobjects")) {
-			CONFIGURATION.addAnnotatedClass(annotatedClass);
-		}
+		CONFIGURATION.addAnnotatedClass(ResourceCore.class);
+        CONFIGURATION.addAnnotatedClass(TestCore.class);
+        CONFIGURATION.addAnnotatedClass(CourseSectionCore.class);
+        CONFIGURATION.addAnnotatedClass(CourseCore.class);
 	}
 
 	private static Properties makeCommonProperties() {
@@ -37,63 +42,12 @@ public class HibernateConfiguration {
 	}
 
 	@SuppressWarnings("deprecation")
-	public static SessionFactory buildSessionFactory(String login, String password, String connectionURL, Boolean dropBeforeCreate) {
+	public static SessionFactory buildSessionFactory(String login, String password, String connectionURL, Boolean dropBeforeCreate) throws HibernateException {
 		CONFIGURATION.setProperty("hibernate.connection.url", connectionURL + ";DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=TRUE");
 		CONFIGURATION.setProperty("hibernate.connection.username", login);
 		CONFIGURATION.setProperty("hibernate.connection.password", password);
 		CONFIGURATION.setProperty("hibernate.hbm2ddl.auto", dropBeforeCreate ? "create" : "update");
 
 		return CONFIGURATION.buildSessionFactory();
-	}
-
-	private static List<Class<?>> getClassesInPackage(String packageName) {
-		String path = makePathFromPackage(packageName);
-		URL resource = ClassLoader.getSystemClassLoader().getResource(path);
-		if (resource == null) {
-			throw new RuntimeException("Unexpected problem: No resource for " + path);
-		}
-
-		return getClasses(new File(resource.getPath()), packageName);
-	}
-
-	private static String makePathFromPackage(String packageName) {
-		return packageName.replace('.', '/');
-	}
-
-	private static List<Class<?>> getClasses(File directory, String packageName) {
-		List<Class<?>> classes = new ArrayList<>();
-
-		for (String fileName : directory.list()) {
-			String className = null;
-
-			if (fileName.endsWith(".class")) {
-				className = packageName + '.' + fileName.substring(0, fileName.length() - 6);
-			}
-			if (className != null) {
-				classes.add(loadClass(className));
-			}
-		}
-
-//		return classes;
-		List<Class<?>> result = new ArrayList<Class<?>>();
-		try {
-			result.add(Class.forName(packageName + ".ResourceCore"));
-			result.add(Class.forName(packageName + ".TestCore"));
-			result.add(Class.forName(packageName + ".CourseSectionCore"));
-			result.add(Class.forName(packageName + ".CourseCore"));
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-		}
-		return result;
-	}
-
-	private static Class<?> loadClass(String className) {
-		try {
-			return Class.forName(className);
-		}
-		catch (ClassNotFoundException e) {
-			throw new RuntimeException("Unexpected ClassNotFoundException loading class '" + className + "'");
-		}
 	}
 }
