3831dd6914a8b5cc0825df613f249cc658259baf
petrishchev <s.s.petrishchev@yandex.ru>
2013-12-10 17:06:29 +0400

    Доделана имплементация in memory dao

--- in-memory-dao/src/main/java/org/schooldesk/dao/inmemory/InMemoryAbstractDao.java
+++ in-memory-dao/src/main/java/org/schooldesk/dao/inmemory/InMemoryAbstractDao.java
@@ -2,57 +2,61 @@ package org.schooldesk.dao.inmemory;
 
 import org.schooldesk.dao.DataAccessException;
 import org.schooldesk.dao.IDao;
+import org.schooldesk.dao.inmemory.inmemory.Database;
 import org.schooldesk.dto.IDto;
 
+import java.util.HashSet;
 import java.util.Set;
 
 
 public abstract class InMemoryAbstractDao<T extends IDto> implements IDao<T> {
 	@Override
 	public T save(T entity) throws DataAccessException {
-		// TODO: implement me
-		throw new UnsupportedOperationException();
+		Database.database.get(getClass()).put(entity.getId(), entity);
+		return  entity;
 	}
 
 	@Override
 	public Set<T> save(Set<T> entities) throws DataAccessException {
-		// TODO: implement me
-		throw new UnsupportedOperationException();
+		for (T entity : entities){
+			save(entity);
+		}
+		return entities;
 	}
 
 	@Override
 	public void update(T entity) throws DataAccessException {
-		// TODO: implement me
-		throw new UnsupportedOperationException();
+		Database.database.get(getClass()).put(entity.getId(), entity);
 	}
 
 	@Override
 	public void update(Set<T> entities) throws DataAccessException {
-		// TODO: implement me
-		throw new UnsupportedOperationException();
+		for (T entity : entities){
+			update(entity);
+		}
 	}
 
 	@Override
 	public T loadById(Long id) throws DataAccessException {
-		// TODO: implement me
-		throw new UnsupportedOperationException();
+		return (T) Database.database.get(getClass()).get(id);
 	}
 
 	@Override
 	public Set<T> loadByIds(Set<Long> ids) throws DataAccessException {
-		// TODO: implement me
-		throw new UnsupportedOperationException();
+		Set<T> result = new HashSet<>();
+		for (Long id : ids){
+			result.add(loadById(id));
+		}
+		return result;
 	}
 
 	@Override
 	public Set<T> loadAll() throws DataAccessException {
-		// TODO: implement me
-		throw new UnsupportedOperationException();
+		return (Set<T>) Database.database.get(getClass()).values();
 	}
 
 	@Override
 	public void delete(Long id) throws DataAccessException {
-		// TODO: implement me
-		throw new UnsupportedOperationException();
+		Database.database.get(getClass()).remove(id);
 	}
 }
--- in-memory-dao/src/main/java/org/schooldesk/dao/inmemory/inmemory/Database.java
+++ in-memory-dao/src/main/java/org/schooldesk/dao/inmemory/inmemory/Database.java
@@ -1,5 +1,6 @@
 package org.schooldesk.dao.inmemory.inmemory;
 
+import org.schooldesk.dao.inmemory.*;
 import org.schooldesk.dto.IDto;
 import org.schooldesk.dto.inmemory.*;
 
@@ -8,20 +9,26 @@ import java.util.Map;
 
 
 public class Database {
-	public static Map<Long, ? extends IDto> courses = new HashMap<>();
-	public static Map<Long, ? extends IDto> courseSections = new HashMap<>();
-	public static Map<Long, ? extends IDto> educationStages = new HashMap<>();
-	public static Map<Long, ? extends IDto> tests = new HashMap<>();
-	public static Map<Long, ? extends IDto> testAnswers = new HashMap<>();
-	public static Map<Long, ? extends IDto> testQuestions = new HashMap<>();
-	public static Map<Long, ? extends IDto> testResults = new HashMap<>();
-	public static Map<Long, ? extends IDto> users = new HashMap<>();
-	public static Map<Long, ? extends IDto> userTestAnswers = new HashMap<>();
+	public static Map<Long, IDto> courses = new HashMap<>();
+	public static Map<Long, IDto> courseSections = new HashMap<>();
+	public static Map<Long, IDto> educationStages = new HashMap<>();
+	public static Map<Long, IDto> tests = new HashMap<>();
+	public static Map<Long, IDto> testAnswers = new HashMap<>();
+	public static Map<Long, IDto> testQuestions = new HashMap<>();
+	public static Map<Long, IDto> testResults = new HashMap<>();
+	public static Map<Long, IDto> users = new HashMap<>();
+	public static Map<Long, IDto> userTestAnswers = new HashMap<>();
 
-	public static Map<Class<? extends IDto>, Map<Long, ? extends IDto>> database = new HashMap<Class<? extends IDto>, Map<Long, ? extends IDto>>(){{
-			put(InMemoryCourse.class, courses);
-			put(InMemoryCourseSection.class, courseSections);
-			put(InMemoryEducationStage.class, educationStages);
-		}
+	public static Map<Class<? extends InMemoryAbstractDao>, Map<Long, IDto>> database = new HashMap<Class<? extends InMemoryAbstractDao>, Map<Long, IDto>>(){{
+			put(InMemoryCourseDao.class, courses);
+			put(InMemoryCourseSectionDao.class, courseSections);
+			put(InMemoryEducationStageDao.class, educationStages);
+			put(InMemoryTestDao.class, tests);
+			put(InMemoryTestAnswerDao.class, testAnswers);
+			put(InMemoryTestQuestionDao.class, testQuestions);
+			put(InMemoryTestResultDao.class, testResults);
+			put(InMemoryUserDao.class, users);
+			put(InMemoryUserTestAnswerDao.class, userTestAnswers);
+	}
 	};
 }
